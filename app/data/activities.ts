7451354175ce4b41e0831fc664ea47e5465c7a23
vachiravitz <vachiravit.tunt@bumail.net>
2026-07-24 01:16:46 +0700

    Fix activity image paths

--- app/data/activities.ts
+++ app/data/activities.ts
@@ -8,7 +8,7 @@ export const activities = [
     role: "",
     description: "Competitive with all student in teachnology of innovation major ",
     result: "2nd Place",
-    image: "images\activities\activity1.jpg",
+    image: "/images/activities/activity1.jpg",
   },
   {
     type: "Cyber Fortress Challenge & Career Expo",
@@ -18,7 +18,6 @@ export const activities = [
     role: "",
     description: "Explore the various booths and learn about their operations, including the Cyber ​​activities.",
     result: "Supported 40+ participants",
-    image: "images\activities\activity2.jpg",
+    image: "/images/activities/activity2.jpg",
   },
 ];
-
